bc965f6dfc43bb00fdc510f86bb3dc8d6f4f31f2
sandrine <sandrinezocca@gmail.com>
2019-05-11 15:58:10 +0200

    modification form

--- src/components/AdminForm.js
+++ src/components/AdminForm.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
-const AdminForm = () => {
+const AdminForm = ({
+	id: key,
+	majRecette,
+	recettes
+}) => {
+	const recette = recettes[key]
+
+	const handleChange = (event, key) => {
+		const { name, value } = event.target
+		const recette = recettes[key]
+		recette[name] = value
+		majRecette(key, recette)
+	}
+
+	
 	return (
 		<div className="card">
 			<form className="admin-form">
-				<input type="text" name="nom" placeholder="Nom de la recette" />
-				<input type="text" name="image" placeholder="Adresse de l'image" />
-				<textarea name="ingredients" rows="3" placeholder="Liste des ingrédients" ></textarea>
-				<textarea name="instructions" rows="15" placeholder="Liste des instructions" ></textarea>
+				<input value={recette.nom} onChange={e => handleChange(e, key)} type="text" name="nom" placeholder="Nom de la recette" />
+				<input value={recette.image} onChange={e => handleChange(e, key)} type="text" name="image" placeholder="Adresse de l'image" />
+				<textarea value={recette.ingredients} onChange={e => handleChange(e, key)} name="ingredients" rows="3" placeholder="Liste des ingrédients" ></textarea>
+				<textarea value={recette.instructions} onChange={e => handleChange(e, key)} name="instructions" rows="15" placeholder="Liste des instructions" ></textarea>
 			</form>
 			<button>Supprimer</button>
 		</div>
