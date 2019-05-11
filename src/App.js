4badbca8f155d3b5bb16c0bf2f7c94b573504eef
sandrine <sandrinezocca@gmail.com>
2019-05-11 13:36:35 +0200

    show state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Header from './components/Header';
 import recettes from './recettes';
 import Admin from './components/Admin';
+import Card from './components/Card';
 
 class App extends Component {
 
@@ -21,14 +22,15 @@ class App extends Component {
   chargerExample = () => this.setState({ recettes })
 
 
-  render() { 
+  render() {
+    const cards = Object.keys(this.state.recettes)
+      .map(key => <Card  key={ key } details={ this.state.recettes[key] }></Card> )
+    
     return ( 
       <div className='box'>
         <Header pseudo={ this.state.pseudo }/>
         <div className='cards'>
-          <div className='card'>
-            <h2>Une Carte</h2>
-          </div>
+          { cards }
         </div>
         <Admin
           chargerExample={ this.chargerExample }
