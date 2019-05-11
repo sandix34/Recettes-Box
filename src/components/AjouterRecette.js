0bc3ce85561ecad2c5cd4c4fb36c67f034d945b8
sandrine <sandrinezocca@gmail.com>
2019-05-11 14:48:17 +0200

    add form

--- src/components/AjouterRecette.js
+++ src/components/AjouterRecette.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 
 class AjouterRecette extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div>
-                <h2>Ajouter Recette</h2>
-            </div>
-         );
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			nom: '',
+			image: '',
+			ingredients: '',
+			instructions: ''
+		}
+	}
+	render() {
+		return (
+			<div className="card">
+				<form  className="admin-form ajouter-rectee">
+					<input name="nom" type="text" placeholder="nom de la recette"/>
+					<input name="image" type="text" placeholder="nom de l'image"/>
+					<textarea name="ingredients" rows="3" placeholder="liste des ingrédients"></textarea>
+					<textarea name="instructions" rows="15" placeholder="liste des instruction"></textarea>
+				</form>
+			</div>
+		);
+	}
 }
- 
+
 export default AjouterRecette;
