6d4f03d5a7ff1c333d0a81af4e3b9d3245a8d644
sandrine <sandrinezocca@gmail.com>
2019-05-11 14:02:01 +0200

    dynamic image

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import { link } from 'fs';
 
 const Card = ({ details }) => {
 
+	//format ingredients list
 	const ingredients = details.ingredients
 		.split(',')
 		.map(item => <li key={item}>{ item }</li>)
 
+	// format instruction list	
 	const instructions = details.instructions
 		.split('\n')	
-		.map(item => <li key={item}>{ item }</li>)	 
+		.map(item => <li key={item}>{ item }</li>)
+	
+	// dynamic image
+	const requireImage = chemin => {
+		try {
+			return require(`../img/${chemin}`)
+		} catch (err) {
+			return require(`../img/default.jpeg`)
+		}
+	}	
 
 	return (
 		<div className="card">
 		<div className="image">
-			<img src={require(`../img/${ details.image }`)} alt={ details.nom }/>
+			<img src={requireImage(details.image)} alt={ details.nom }/>
 		</div>
 			<div className="recette">
 				<h2>{ details.nom }</h2>
