9f022d2c7cc7af91923449c32cb95fbd5a1533ef
sandrine <sandrinezocca@gmail.com>
2019-05-11 15:09:03 +0200

    modify the state of the form

--- src/components/AjouterRecette.js
+++ src/components/AjouterRecette.js
@@ -16,14 +16,26 @@ class AjouterRecette extends Component {
 			this.setState({[name]: value})
 	}
 
+	handleSubmit = event => {
+		event.preventDefault()
+		const recette = { ...this.state }
+		this.props.ajouterRecette(recette)
+		// Reset
+		Object.keys(recette).forEach(item => {
+			recette[item] = ''
+		})
+		this.setState({ ...recette })
+	}
+
 	render() {
 		return (
 			<div className="card">
-				<form  className="admin-form ajouter-rectee">
+				<form  className="admin-form ajouter-rectee" onSubmit={this.handleSubmit}>
 					<input value={this.state.nom} onChange={this.handleCchange} name="nom" type="text" placeholder="nom de la recette"/>
 					<input value={this.state.image} onChange={this.handleCchange} name="image" type="text" placeholder="nom de l'image"/>
 					<textarea value={this.state.ingredients} onChange={this.handleCchange} name="ingredients" rows="3" placeholder="liste des ingrédients"></textarea>
 					<textarea value={this.state.instructions} onChange={this.handleCchange} name="instructions" rows="15" placeholder="liste des instruction"></textarea>
+					<button type="submit">+ Ajouter uen recette </button>
 				</form>
 			</div>
 		);
